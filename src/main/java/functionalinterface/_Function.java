e0d619388766ffa795c76b6b96cb2dbc546c8dae
Mostafa Srour <mostafa.sroor.1983@gmail.com>
2021-02-14 11:12:14 +0200

    channing in functional programming

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -10,10 +10,16 @@ public class _Function {
       int j = incrementByOneFunction.apply(0);
         System.out.println(i);
 
+        final Function<Integer, Integer> channingFunction = incrementByOneFunction.andThen(multiplyByOneFunction);
+        int k = channingFunction.apply(1);
+        System.out.println(k);
     }
 
+    // multiply method using functional interface
+    static Function<Integer,Integer> multiplyByOneFunction = k ->  k*10;
+
     // increment method using functional interface
-    static Function<Integer,Integer> incrementByOneFunction = j ->  j +1;
+    static Function<Integer,Integer> incrementByOneFunction = j ->  j + 1;
 
     static int incrementByOne(int i){
           return  i + 1;
