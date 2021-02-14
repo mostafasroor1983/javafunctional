f3b2c26f2433e40498a4c75845391427557247cd
Mostafa Srour <mostafa.sroor.1983@gmail.com>
2021-02-14 12:37:28 +0200

    BiConsumer Functional Interface

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,8 +1,9 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-//Represents an operation that accepts a single input argument and returns no result.
+
 public class _Consumer {
     public static void main(String[] args) {
         final Customer mostafa_sroor = new Customer("Mostafa Sroor", "70259437");
@@ -11,14 +12,27 @@ public class _Consumer {
 
         //Consumer Functional Interface
         greetingCustomerConsumer.accept(mostafa_sroor);
+
+        //BiConsumer Functional Interface
+        greetingCustomerConsumerV2.accept(mostafa_sroor, false);
+        greetingCustomerConsumerV2.accept(mostafa_sroor, true);
     }
 
     static void greetingCutomer(Customer customer) {
-        System.out.println("Hello " + customer.getName() + " thanks for registering with phone number " + customer.getPhone());
+        System.out.println("Hello " + customer.getName() + " thanks for registering phone number " + customer.getPhone());
     }
 
+    //Represents an operation that accepts a single input argument and returns no result.
     static Consumer<Customer> greetingCustomerConsumer =
-            (customer) -> System.out.println("Hello " + customer.getName() + " thanks for registering with phone number " + customer.getPhone());
+            (customer) -> System.out.println("Hello " + customer.getName() + " thanks for registering phone number " + customer.getPhone());
+
+    //Represents an operation that accepts two input arguments and returns no result.
+    static BiConsumer<Customer, Boolean> greetingCustomerConsumerV2 =
+            (customer, showPhoneNumber) -> {
+                System.out.println("Hello " + customer.getName() +
+                        " thanks for registering phone number " +
+                        (showPhoneNumber ? customer.getPhone() : "********"));
+            };
 
     static class Customer {
         private String name;
