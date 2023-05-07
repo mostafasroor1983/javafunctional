ebcba6a8569f15cc4de17381bffae27de409c6b1
Mostafa Srour <mostafa.sroor.1983@gmail.com>
2023-05-07 17:30:59 +0400

    Callback java like java script 2

--- src/main/java/javascript/Callback.java
+++ src/main/java/javascript/Callback.java
@@ -6,6 +6,7 @@ public class Callback {
     public static void main(String[] args) {
         Consumer<String> consumer = value -> System.out.println("Hello, " + value);
         hello("Mostafa", null, consumer);
+        hello2("Mostafa", null, () -> System.out.println("lastName isn't provided."));
     }
 
     static void hello(String firstName, String lastName, Consumer<String> consumer) {
@@ -16,6 +17,17 @@ public class Callback {
             System.out.println(lastName);
         } else {
             consumer.accept(firstName);
+        }
+    }
+
+    static void hello2(String firstName, String lastName, Runnable runnable) {
+        if (firstName != null) {
+            System.out.println(firstName);
+        }
+        if (lastName != null) {
+            System.out.println(lastName);
+        } else {
+            runnable.run();
         }
     }
 
